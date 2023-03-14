3e64faf14ea2979cbfc4233a0173da5b5ecab7cb
raaaaj214 <raaaaj214@users.noreply.github.com>
2023-03-14 09:58:45 +0530

    omk

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,17 +8,17 @@ export default function Navbar(props) {
   return (
     <nav className={`navbar navbar-expand-lg navbar-${props.mode.backgroundColor} bg-${props.mode.backgroundColor} `}>
   <div className="container-fluid">
-    <a href="#" className={`navbar-brand text-${props.mode.color}`} href="/">{props.title}</a>
+    <a  className={`navbar-brand text-${props.mode.color}`} href="/">{props.title}</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a  href="#" className={`nav-link active text-${props.mode.color}`} aria-current="page" href="/">{props.firstValue}</a>
+          <a className={`nav-link active text-${props.mode.color}`} aria-current="page" href="/">{props.firstValue}</a>
         </li>
         <li className="nav-item">
-          <a href="#" className={`nav-link active text-${props.mode.color}`} href="/">{props.secondValue}</a>
+          <a className={`nav-link active text-${props.mode.color}`} href="/">{props.secondValue}</a>
         </li>
       </ul>
       {/*
